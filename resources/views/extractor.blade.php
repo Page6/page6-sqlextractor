fba773f5c4d8d2bcde4869579516b5864260d838
Page6 <yangchaoyu90@hotamil.com>
2018-09-03 16:54:50 +0800

    change style: extractor

--- resources/views/extractor.blade.php
+++ resources/views/extractor.blade.php
@@ -1,23 +1,32 @@
-<html>
-	<head>
-		<title>数据提取</title>
-	</head>
-	<body>
-		<table border=1>
-			<tr>
-				<td>ID号</td>
-				<td>姓名</td>
-				<td>年龄</td>
-				<td>职位</td>
-			</tr>
-			@foreach ($employees as $employee)
-			<tr>
-				<td>{{ $employee->id }}</td>
-				<td>{{ $employee->name }}</td>
-				<td>{{ $employee->age }}</td>
-				<td>{{ $employee->position }}</td>
-			</tr>
-			@endforeach
-		</table>
-	</body>
-</html>
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading">Dashboard</div>
+
+                <div class="panel-body">
+                    <table border=1>
+						<tr>
+							<td>ID号</td>
+							<td>姓名</td>
+							<td>年龄</td>
+							<td>职位</td>
+						</tr>
+						@foreach ($employees as $employee)
+						<tr>
+							<td>{{ $employee->id }}</td>
+							<td>{{ $employee->name }}</td>
+							<td>{{ $employee->age }}</td>
+							<td>{{ $employee->position }}</td>
+						</tr>
+						@endforeach
+					</table>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
