39851a7b645acb2dfb611f8a4b5b24fe0fb585ba
Page6 <yangchaoyu90@hotamil.com>
2018-09-04 16:24:08 +0800

    add migration: records

---
+++ database/migrations/2018_09_04_161716_create_records_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRecordsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('records', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('user');
+            $table->string('sql');
+            $table->timestamp('extracted_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('records');
+    }
+}
