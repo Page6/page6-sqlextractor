7a7bd15fe15b2dd9e7817fb8b60e1a504c9d9a47
Page6 <yangchaoyu90@hotamil.com>
2018-09-04 16:12:40 +0800

    change function: DB

--- app/Http/Controllers/ExtractorController.php
+++ app/Http/Controllers/ExtractorController.php
@@ -3,7 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
+//use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\Controller;
 
 class ExtractorController extends Controller
@@ -14,10 +15,13 @@ class ExtractorController extends Controller
      * extract data from database.
      */
     public function extract() {
-    	//DB::connection()->enableQueryLog();
-    	$employees = DB::select('select * from employee');
-    	//DB::listen(function($query) { Log::info($query); });
-    	//dd(DB::getQueryLog());
+    	\DB::enableQueryLog();
+    	//$employees = DB::select('select * from employee');
+        $employees = \DB::table('employee')->get();
+
+        $log = \DB::getQueryLog()[0]['query'];
+        $record=array('user'=>Auth::user()->name,'sql'=>$log,'extracted_at'=>date('Y-m-d h:i:s',time()));
+        $result = \DB::table('records')->insert($record);
 
     	return view('extractor', ['employees'=>$employees]);
     }
