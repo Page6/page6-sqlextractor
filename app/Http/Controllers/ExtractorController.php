59456641ff4da03dd7b71fa3a5b78f4e51edf5bb
Page6 <yangchaoyu90@hotmail.com>
2018-09-09 22:55:58 +0800

    import sql functions

--- app/Http/Controllers/ExtractorController.php
+++ app/Http/Controllers/ExtractorController.php
@@ -16,13 +16,51 @@ class ExtractorController extends Controller
      */
     public function extract() {
     	//\DB::enableQueryLog();
-    	//$employees = DB::select('select * from employee');
-        $employees = \DB::connection('mysql_page6')->table('employees')->get();
+    	$employees = \DB::select('select * from employees');
+      //$employees = \DB::connection('mysql_page6')->table('employees')->get();
+      $results_visit = \DB::connection('mysql_page6')->select("SELECT b.name 科室, COUNT(a.patient_id) 挂号人次
+          FROM mz_visit_table a, unit_code b
+          WHERE a.visit_date >= '2018-09-02'
+            AND a.visit_date < '2018-09-04'
+            AND a.visit_flag <> '9'
+            AND a.gh_date is not null
+            AND a.visit_dept = b.code
+          GROUP BY b.name
+          ORDER BY b.name");
+      $results_fee = \DB::connection('mysql_page6')->select("SELECT b.name 科室, SUM(a.charge_price) 总费用
+          FROM mz_detail_charge a, unit_code b
+          WHERE a.price_date >= '2018-09-02'
+            AND a.price_date < '2018-09-04'
+            AND a.charge_status >= 2
+            AND a.apply_unit = b.code
+          GROUP BY b.name
+          ORDER BY b.name");
+      $results_drag = \DB::connection('mysql_page6')->select("SELECT b.name 科室, SUM(a.charge_price) 药费用
+          FROM mz_detail_charge a, unit_code b
+          WHERE a.price_date >= '2018-09-02'
+            AND a.price_date < '2018-09-04'
+            AND a.charge_status >= 2
+            AND a.apply_unit = b.code
+            AND a.bill_code in('001','005','006')
+          GROUP BY b.name
+          ORDER BY b.name");
+      $results_material = \DB::connection('mysql_page6')->select("SELECT b.name 科室, SUM(a.charge_price) 材料费用
+          FROM mz_detail_charge a, unit_code b
+          WHERE a.price_date >= '2018-09-02'
+            AND a.price_date < '2018-09-04'
+            AND a.charge_status >= 2
+            AND a.apply_unit = b.code
+            AND a.bill_code = '016'
+          GROUP BY b.name
+          ORDER BY b.name");
 
-        //$log = \DB::getQueryLog()[0]['query'];
-        $record=array('user'=>Auth::user()->name,'sql'=>'extract-select','extracted_at'=>date('Y-m-d H:i:s',time()));
-        $result = \DB::table('records')->insert($record);
+      $results = $results_visit;
+      dd($results);
 
-    	return view('extractor', ['employees'=>$employees]);
+      //$log = \DB::getQueryLog()[0]['query'];
+      $record=array('user'=>Auth::user()->name,'sql'=>'extract-select','extracted_at'=>date('Y-m-d H:i:s',time()));
+      $records = \DB::table('records')->insert($record);
+
+    	return view('extractor', ['results'=>$results]);
     }
 }
