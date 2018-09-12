d3d72c69b8923e45a59119fb78b7ee8b7c343e91
Page6 <yangchaoyu90@hotamil.com>
2018-09-12 11:11:08 +0800

    hide element: remeber me

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -39,7 +39,7 @@
                             </div>
                         </div>
 
-                        <div class="form-group">
+                        <!-- <div class="form-group">
                             <div class="col-md-6 col-md-offset-4">
                                 <div class="checkbox">
                                     <label>
@@ -47,7 +47,7 @@
                                     </label>
                                 </div>
                             </div>
-                        </div>
+                        </div> -->
 
                         <div class="form-group">
                             <div class="col-md-8 col-md-offset-4">
